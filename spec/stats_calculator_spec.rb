813ef258f71c686d70b3cac8cb227db76166f1f8
Chloe <chloe.wong@madetech.com>
2020-09-25 10:32:49 +0100

    added README file

---
+++ spec/stats_calculator_spec.rb
@@ -0,0 +1,10 @@
+require './stats_calculator'
+
+# //Arrange block (aka Given) – create all necessary preconditions
+# var fizzBuzzer = new FizzBuzzer()
+# //Act block (aka When) – execute the subject under test
+# var fizzBuzzed = fizzBuzzer.FizzBuzz(1)
+# //Assert block (aka Then) – assert the expected results have occurred
+# assert fizzBuzzed == "1"
+
+describe ""
