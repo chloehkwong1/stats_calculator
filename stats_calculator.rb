66f98cc50a82dc88c8601969122d9a06802a387c
Chloe <chloe.wong@madetech.com>
2020-09-26 10:53:28 +0100

    refactored minimum value function to avoid duplication

--- stats_calculator.rb
+++ stats_calculator.rb
@@ -5,13 +5,7 @@ class StatsCalc
     end
 
     def minimum_value(array_input)
-        if array_input == [1, 2]
-            return "minimum value = 1"
-        end
-        if array_input == [3, 2]
-            return "minimum value = 2"
-        end
-        "minimum value = 13"
+        "minimum value = #{array_input.min}"
     end
 
 end
