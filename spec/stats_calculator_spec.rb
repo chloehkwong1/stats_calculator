e8c69460ab46b10d9a969d3759ec1a1329cc1b20
Chloe <chloe.wong@madetech.com>
2020-09-27 17:17:33 +0100

    deleted irrelevant tests

--- spec/stats_calculator_spec.rb
+++ spec/stats_calculator_spec.rb
@@ -11,38 +11,6 @@ describe StatsCalc do
     let(:stats_calc) {StatsCalc.new}
 
     context "tests for an input of array size 1" do
-        describe "given an array with an element of 1" do
-            it "will return an 1 as an integer" do
-                expect(stats_calc.stats_calculator([1])).to eq(1)
-            end
-        end
-
-        describe "given an array with an element of 2" do
-            it "will return a 2 as an integer" do
-                expect(stats_calc.stats_calculator([2])).to eq(2)
-            end
-        end
-
-        describe "given an array with an element of 3" do
-            it "will return a 3 as an integer" do
-                expect(stats_calc.stats_calculator([3])).to eq(3)
-            end
-        end
-    end
-
-    context "for an array of size 2" do
-        describe "given an array with two elements [1, 2]" do
-            it "will return the minimum value of 1" do
-                expect(stats_calc.stats_calculator([1, 2])).to eq(1)
-            end
-        end
-
-        describe "given an array with two elements [2, 1]" do
-            it "will return the minimum value of 1" do
-                expect(stats_calc.stats_calculator([2, 1])).to eq(1)
-            end
-        end
-
         describe "given an array with two elements [1, 2]" do
             it "will return the minimum of 1 in a string" do
                 expect(stats_calc.minimum_value([1, 2])).to eq("minimum value = 1")
--- stats_calculator.rb
+++ stats_calculator.rb
@@ -1,7 +1,7 @@
 class StatsCalc
 
     def stats_calculator(array_input)
-        array_input.min
+        
     end
 
     def minimum_value(array_input)
