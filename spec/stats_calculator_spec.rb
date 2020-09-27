53e8c36f6ffd7b7a846317ff610d05ac8bd22c94
Chloe <chloe.wong@madetech.com>
2020-09-27 23:41:09 +0100

    refactored to incorporate initialize, still needs a bit more refactoring

--- spec/stats_calculator_spec.rb
+++ spec/stats_calculator_spec.rb
@@ -8,36 +8,40 @@ require './stats_calculator'
 # assert fizzBuzzed == "1"
 
 describe StatsCalc do
-    let(:stats_calc) {StatsCalc.new}
 
     context "tests for an input of array size 1" do
         describe "given an array with two elements [1, 2]" do
             it "will return the minimum of 1 in a string" do
-                expect(stats_calc.minimum_value([1, 2])).to eq("minimum value = 1")
+                stats_calc = StatsCalc.new([1, 2])
+                expect(stats_calc.minimum_value).to eq("minimum value = 1")
             end
         end
 
         describe "given an array with two elements [3, 2]" do
             it "will return the minimum of 2 in a string" do
-                expect(stats_calc.minimum_value([3, 2])).to eq("minimum value = 2")
+                stats_calc = StatsCalc.new([3, 2])
+                expect(stats_calc.minimum_value).to eq("minimum value = 2")
             end
         end
 
         describe "given an array with two elements [13, 17]" do
             it "will return the minimum of 2 in a string" do
-                expect(stats_calc.minimum_value([13, 17])).to eq("minimum value = 13")
+                stats_calc = StatsCalc.new([13, 17])
+                expect(stats_calc.minimum_value).to eq("minimum value = 13")
             end
         end
 
         describe "given an array with two elements [1, 2]" do
             it "will return a maximum of 2 in a string" do
-                expect(stats_calc.maximum_value([1, 2])).to eq("maximum value = 2")
+                stats_calc = StatsCalc.new([1, 2])
+                expect(stats_calc.maximum_value).to eq("maximum value = 2")
             end
         end
 
         describe "given an array with two elements [3, 2]" do
             it "will return a maximum of 3 in a string" do
-                expect(stats_calc.maximum_value([3, 2])).to eq("maximum value = 3")
+                stats_calc = StatsCalc.new([3, 2])
+                expect(stats_calc.maximum_value).to eq("maximum value = 3")
             end
         end
     end
@@ -45,19 +49,22 @@ describe StatsCalc do
     context "testing function to return number of elements" do
         describe "given an array of size 1" do
             it "will return a size of 1 in a string" do
-                expect(stats_calc.number_of_elements([1])).to eq("number of elements in sequence = 1")
+                stats_calc = StatsCalc.new([1])
+                expect(stats_calc.number_of_elements).to eq("number of elements in sequence = 1")
             end
         end
 
         describe "given an array of size 2" do
             it "will return a size of 2 in a string" do
-                expect(stats_calc.number_of_elements([1, 2])).to eq("number of elements in sequence = 2")
+                stats_calc = StatsCalc.new([1, 2])
+                expect(stats_calc.number_of_elements).to eq("number of elements in sequence = 2")
             end
         end
 
         describe "given an array of size 3" do
             it "will return a size of 3 in a string" do
-                expect(stats_calc.number_of_elements([1, 2, 3])).to eq("number of elements in sequence = 3")
+                stats_calc = StatsCalc.new([1, 2, 3])
+                expect(stats_calc.number_of_elements).to eq("number of elements in sequence = 3")
             end
         end
     end
@@ -65,25 +72,29 @@ describe StatsCalc do
     context "testing function to return average value of array" do
         describe "given an array with two elements [1, 2]" do
             it "will return 1.5 in a string" do
-                expect(stats_calc.average_value([1, 2])).to eq("average value = 1.5")
+                stats_calc = StatsCalc.new([1, 2])
+                expect(stats_calc.average_value).to eq("average value = 1.5")
             end
         end
 
         describe "given an array with two elements [1, 3]" do
             it "will return 2 in a string" do
-                expect(stats_calc.average_value([1, 3])).to eq("average value = 2.0")
+                stats_calc = StatsCalc.new([1, 3])
+                expect(stats_calc.average_value).to eq("average value = 2.0")
             end
         end
 
         describe "given an array with three elements [1, 2, 3]" do
             it "will return 2 in a string" do
-                expect(stats_calc.average_value([1, 2, 3])).to eq("average value = 2.0")
+                stats_calc = StatsCalc.new([1, 2, 3])
+                expect(stats_calc.average_value).to eq("average value = 2.0")
             end
         end
 
         describe "given an array with three elements [1, 3, 3]" do
             it "will return 2.3 in a string" do
-                expect(stats_calc.average_value([1, 3, 3])).to eq("average value = 2.3")
+                stats_calc = StatsCalc.new([1, 3, 3])
+                expect(stats_calc.average_value).to eq("average value = 2.3")
             end
         end
     end
@@ -91,7 +102,17 @@ describe StatsCalc do
     context "full stats calculator for an array" do
         describe "given an array [1, 2]" do
             it "will return the min/max value, the number of elements, and the average value" do
-                expect(stats_calc.stats_calculator([1, 2])).to eq("minimum value = 1\nmaximum value = 2\nnumber of elements in sequence = 2\naverage value = 1.5")
+                stats_calc = StatsCalc.new([1, 2])
+                expect(stats_calc.stats_calculator).to eq("minimum value = 1\nmaximum value = 2\nnumber of elements in sequence = 2\naverage value = 1.5")
+            end
+        end
+    end
+
+    context "full stats calculator for an array" do
+        describe "given an array [1, 2, 3]" do
+            it "will return the min/max value, the number of elements, and the average value" do
+                stats_calc = StatsCalc.new([1, 2, 3])
+                expect(stats_calc.stats_calculator).to eq("minimum value = 1\nmaximum value = 3\nnumber of elements in sequence = 3\naverage value = 2.0")
             end
         end
     end
--- stats_calculator.rb
+++ stats_calculator.rb
@@ -1,22 +1,27 @@
 class StatsCalc
+    attr_accessor :array_input
 
-    def stats_calculator(array_input)
-        return "#{minimum_value(array_input)}\n#{maximum_value(array_input)}\n#{number_of_elements(array_input)}\n#{average_value(array_input)}"
+    def initialize(array_input)
+        @array_input = array_input
     end
 
-    def minimum_value(array_input)
+    def stats_calculator
+        return "#{minimum_value}\n#{maximum_value}\n#{number_of_elements}\n#{average_value}"
+    end
+
+    def minimum_value
         "minimum value = #{array_input.min}"
     end
 
-    def maximum_value(array_input)
+    def maximum_value
         "maximum value = #{array_input.max}"
     end
 
-    def number_of_elements(array_input)
+    def number_of_elements
         return "number of elements in sequence = #{array_input.size}"
     end
 
-    def average_value(array_input)
+    def average_value
         return "average value = #{(array_input.sum/array_input.size.to_f).round(1)}"
     end
 end
